8f526b23dc2fb0ed110a743bb39410859bc3fbd3
KUSHHPATEL <kushhp1206@gmail.com>
2024-12-13 13:17:28 +0530

    Add files via upload

---
+++ Day 1/intro.py
@@ -0,0 +1,13 @@
+
+a=int(input("enter the number:"))
+b=int(input("enter the number"))
+c=a+b
+if c==30:
+     print("its 30")
+elif c<30:
+    print("its less than 30")
+else:
+    print("its greater than 30")
+                 
+
+   
---
+++ Day 1/list.py
@@ -0,0 +1,33 @@
+list=[]
+n=int(input("enter number of elements: "))
+for i in range (0,n):
+    a=int(input("enter the number: "))
+    list.append(a)
+print(list)
+b=sorted(list)
+print(b)
+
+
+# sum = 0
+# for i in range(0,3):
+#     a = int(input("enter marks:"))
+#     sum += a
+# avg = sum/3
+# print("sum is", sum)
+# print("avg is ",avg)
+# if avg>35:
+#  print("pass")
+# else:
+#  print("fail")
+ 
+ 
+ 
+ 
+# m1 = {}
+# n = int(input("enter number of ele"))
+
+# for i in range(0,n):
+#     a,b = input("enter key value pair data:").split()
+#     m1[a] = [b]
+#     print(m1)
+ 
